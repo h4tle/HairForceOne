9e8025d71ef9dee2fc8d6a862b2a36dacdd210fb
Michael Hatlehol <1010959@ucn.dk>
2020-11-02 16:29:48 +0100

    Models added

---
+++ HairForceOne.WebClient/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HairForceOne.WebClient.Models
+{
+    public class Customer
+    {
+        private List<TimeBooking> _timeBookings; // Kender timebooking kunde eller kender kunde sine timebookings? Begge?
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public List<TimeBooking> TimeBookings
+        {
+            get
+            {
+                return _timeBookings;
+            }
+        }
+
+        public Customer()
+        {
+            _timeBookings = new List<TimeBooking>();
+        }
+    }
+}
