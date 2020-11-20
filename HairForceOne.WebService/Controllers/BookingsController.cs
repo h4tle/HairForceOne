de267d6f6eb2c3984006dafa0428a1711536047e
Michael Hatlehol <1010959@ucn.dk>
2020-11-20 14:18:30 +0100

    Bookingcontroller

--- HairForceOne.WebService/Controllers/BookingsController.cs
+++ HairForceOne.WebService/Controllers/BookingsController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Dapper;
+using HairForceOne.WebService.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -10,31 +14,61 @@ namespace HairForceOne.WebService.Controllers
     public class BookingsController : ApiController
     {
         // GET: api/Bookings
-        public IEnumerable<string> Get()
+        public IEnumerable<Booking> GetAllBookings()
         {
-            return new string[] { "value1", "value2" };
+            string sql = "SELECT * FROM hfo_Booking";
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Hildur"].ConnectionString))
+            {
+                return connection.Query<Booking>(sql).ToList();
+            }
         }
-
-        // GET: api/Bookings/5
-        public string Get(int id)
+        public Booking GetBooking(int id)
         {
-            return "value";
+            string sql = $"select * FROM hfo_Booking WHERE BookingId = @BookingId";
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Hildur"].ConnectionString))
+            {
+                return connection.QuerySingleOrDefault<Booking>(sql, new { ProductId = id });
+            }
         }
-
-        // POST: api/Bookings
-        public void Post([FromBody]string value)
+        public HttpResponseMessage Post([FromBody] Booking b, int UserId, int EmployeeId, ProductId[], ServiceId )
         {
-            
+            string sql = "INSERT INTO hfo_Booking (StartTime,EndTime,TotalPrice,Comment)" +
+                         "VALUES (@Brand, @Title, @StartTime, @EndTime, @TotalPrice, @Comment)";
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Hildur"].ConnectionString))
+            {
+                var affectedRows = connection.Execute(sql, new
+                {
+                    StartTime = b.StartTime,
+                    EndTime = b.EndTime,
+                    TotalPrice = b.TotalPrice,
+                    Comment = b.Comment
+                });
+                return Request.CreateResponse(HttpStatusCode.Accepted);
+            }
         }
-
-        // PUT: api/Bookings/5
-        public void Put(int id, [FromBody]string value)
+        public int Put(Booking b)
         {
+            string sql = $"UPDATE hfo_Booking SET StartTime = @StartTime, EndTime = @EndTime, TotalPrice = @TotalPrice, Comment = @Comment WHERE BookingId = @BookingId";
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Hildur"].ConnectionString))
+            {
+                int BookingId = connection.Execute(sql, new
+                {
+                    ProductId = b.BookingId,
+                    StartTime = b.StartTime,
+                    EndTime = b.EndTime,
+                    TotalPrice = b.TotalPrice,
+                    Comment = b.Comment
+                });
+                return BookingId;
+            }
         }
-
-        // DELETE: api/Bookings/5
         public void Delete(int id)
         {
+            string sql = $"DELETE FROM hfo_Booking WHERE BookingId = @BookingId";
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Hildur"].ConnectionString))
+            {
+                connection.Execute(sql, new { ProductId = id });
+            }
         }
     }
 }
---
+++ HairForceOne.WebService/Models/Booking.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HairForceOne.WebService.Models
+{
+	public class Booking
+	{
+		public int BookingId { get; set; }
+		public DateTime StartTime { get; set; }
+		public DateTime EndTime { get; set; }
+		public decimal TotalPrice { get; set; }
+		public int UserId { get; set; }
+		public int EmployeeId { get; set; }
+		public int ProductId { get; set; }
+		public int ServiceId { get; set; }
+		public string Comment { get; set; }
+
+		public Booking(int BookingId_, DateTime StartTime_, DateTime EndTime_, decimal TotalPrice_, int UserId_, int EmployeeId_, int ProductId_, int ServiceId_, string Comment_)
+		{
+			this.BookingId = BookingId_;
+			this.StartTime = StartTime_;
+			this.EndTime = EndTime_;
+			this.TotalPrice = TotalPrice_;
+			this.UserId = UserId_;
+			this.EmployeeId = EmployeeId_;
+			this.ProductId = ProductId_;
+			this.ServiceId = ServiceId_;
+			this.Comment = Comment_;
+		}
+	}
+}
