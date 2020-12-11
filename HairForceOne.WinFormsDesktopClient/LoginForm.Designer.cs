a5ea78e0e60a5a5b21f56bc89b6ee002b3accb21
Michael Hatlehol <1010959@ucn.dk>
2020-12-11 22:36:37 +0100

    ....

--- HairForceOne.WinFormsDesktopClient/LoginForm.Designer.cs
+++ HairForceOne.WinFormsDesktopClient/LoginForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             this.btn_login = new System.Windows.Forms.Button();
             this.lbl_header = new System.Windows.Forms.Label();
             this.lbl_email = new System.Windows.Forms.Label();
@@ -44,10 +45,9 @@
             this.btn_login.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(167)))), ((int)(((byte)(122)))));
             this.btn_login.FlatAppearance.BorderSize = 0;
             this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_login.Location = new System.Drawing.Point(765, 375);
-            this.btn_login.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_login.Location = new System.Drawing.Point(510, 244);
             this.btn_login.Name = "btn_login";
-            this.btn_login.Size = new System.Drawing.Size(129, 40);
+            this.btn_login.Size = new System.Drawing.Size(86, 26);
             this.btn_login.TabIndex = 0;
             this.btn_login.Text = "Log ind";
             this.btn_login.UseVisualStyleBackColor = false;
@@ -59,10 +59,9 @@
             this.lbl_header.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(78)))), ((int)(((byte)(73)))));
             this.lbl_header.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_header.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(167)))), ((int)(((byte)(122)))));
-            this.lbl_header.Location = new System.Drawing.Point(394, 112);
-            this.lbl_header.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_header.Location = new System.Drawing.Point(263, 73);
             this.lbl_header.Name = "lbl_header";
-            this.lbl_header.Size = new System.Drawing.Size(367, 29);
+            this.lbl_header.Size = new System.Drawing.Size(255, 20);
             this.lbl_header.TabIndex = 1;
             this.lbl_header.Text = "Indtast Email og Adgangskode";
             // 
@@ -70,10 +69,9 @@
             // 
             this.lbl_email.AutoSize = true;
             this.lbl_email.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(167)))), ((int)(((byte)(122)))));
-            this.lbl_email.Location = new System.Drawing.Point(266, 203);
-            this.lbl_email.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_email.Location = new System.Drawing.Point(177, 132);
             this.lbl_email.Name = "lbl_email";
-            this.lbl_email.Size = new System.Drawing.Size(52, 20);
+            this.lbl_email.Size = new System.Drawing.Size(35, 13);
             this.lbl_email.TabIndex = 2;
             this.lbl_email.Text = "Email:";
             // 
@@ -81,46 +79,44 @@
             // 
             this.lbl_password.AutoSize = true;
             this.lbl_password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(167)))), ((int)(((byte)(122)))));
-            this.lbl_password.Location = new System.Drawing.Point(266, 277);
-            this.lbl_password.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_password.Location = new System.Drawing.Point(177, 180);
             this.lbl_password.Name = "lbl_password";
-            this.lbl_password.Size = new System.Drawing.Size(112, 20);
+            this.lbl_password.Size = new System.Drawing.Size(76, 13);
             this.lbl_password.TabIndex = 3;
             this.lbl_password.Text = "Adgangskode:";
             this.lbl_password.Click += new System.EventHandler(this.label3_Click);
             // 
             // txt_email
             // 
-            this.txt_email.Location = new System.Drawing.Point(399, 203);
-            this.txt_email.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_email.Location = new System.Drawing.Point(266, 132);
             this.txt_email.Name = "txt_email";
-            this.txt_email.Size = new System.Drawing.Size(493, 26);
+            this.txt_email.Size = new System.Drawing.Size(330, 20);
             this.txt_email.TabIndex = 4;
             // 
             // txt_password
             // 
-            this.txt_password.Location = new System.Drawing.Point(399, 272);
-            this.txt_password.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_password.Location = new System.Drawing.Point(266, 177);
             this.txt_password.Name = "txt_password";
-            this.txt_password.Size = new System.Drawing.Size(493, 26);
+            this.txt_password.Size = new System.Drawing.Size(330, 20);
             this.txt_password.TabIndex = 5;
             this.txt_password.TextChanged += new System.EventHandler(this.txt_password_TextChanged);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(33, 75);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 42);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(208, 306);
+            this.pictureBox1.Size = new System.Drawing.Size(147, 155);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(78)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(928, 448);
+            this.ClientSize = new System.Drawing.Size(619, 291);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txt_password);
             this.Controls.Add(this.txt_email);
@@ -128,7 +124,6 @@
             this.Controls.Add(this.lbl_email);
             this.Controls.Add(this.lbl_header);
             this.Controls.Add(this.btn_login);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
