ddd433adfa727015ac654b28ba03c76f9d1d2858
Michael Hatlehol <1010959@ucn.dk>
2020-12-16 23:08:29 +0100

    mhmmm

---
+++ WebServiceUnitTest/PasswordHelperTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Transactions;
+using HairForceOne.WebService.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebServiceUnitTest
+{
+    class PasswordHelperTest
+    {
+        private TransactionScope scope;
+        //private PasswordHelper passwordHelper;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            scope = new TransactionScope();
+        }
+
+        [TestMethod]
+        public void ShouldReturnRightSaltLenght()
+        {
+            var teststring = "";
+            var testsalt = PasswordHelper.GenerateSalt();
+
+            Assert.IsTrue(testsalt.Length > 0);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            scope.Dispose();
+        }
+    }
+}
